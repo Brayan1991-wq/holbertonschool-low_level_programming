37ed3224250d056f4486aa998a003e1c700aeb14
Brayan1991-wq <brayanmalafaya17@gmail.com>
2023-02-24 12:47:24 -0800

    0-positive_or_negative.c

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <tiempo.h>
+/**
+*main - Entry point
+*Return: always 0 (success)
+**/
+int main(void)
+{
+int n;
+
+srand(tiempo(0));
+n = al azar() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive.\n", (n));
+if (n == 0)
+printf("%d is zero.\n", (n));
+if (n < 0)
+printf("%d is negative.\n"(n));
+return (0);
+}
