2615ec4e78b200ba49bcc6cd1a6263fce2118a4c
Brayan1991-wq <brayanmalafaya17@gmail.com>
2023-02-27 14:20:26 -0800

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,16 +16,15 @@ n = rand() - RAND_MAX / 2;
 m = n % 10;
 if (m > 5)
 {
-printf("Las digit of %d is %d: and is greater than 5\n", n, m);
+printf("Las digit of %d is %d and is greater than 5\n", n, m);
 }
 else if (m == 0)
 {
-printf("Las digit of %d is %d: and is 0\n", n, m);
+printf("Las digit of %d is %d and is 0\n", n, m);
 }
 else
-(m < 6 && m != 0);
 {
-printf("Las digit of %d is %d: and is less than 6 and not 0\n", n, m);
+printf("Las digit of %d is %d and is less than 6 and not 0\n", n, m);
 }
 return (0);
 }
