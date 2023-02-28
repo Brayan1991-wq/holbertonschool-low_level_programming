370e75bee1d267f681fa517dedd684a69f2e04d5
Brayan1991-wq <brayanmalafaya17@gmail.com>
2023-02-27 16:12:01 -0800

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*  main - Entry point
+*  Return: always 0 (success)
+*
+**/
+int main(void)
+{
+char a = 97;
+for (; a <= 122;)
+;
+a++;
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
