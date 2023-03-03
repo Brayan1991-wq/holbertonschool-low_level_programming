e5cdeb2f9ef33cca3294dff69a09760a413c5c1a
Brayan1991-wq <brayanmalafaya17@gmail.com>
2023-03-02 16:06:02 -0800

    7-print_tebahpla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*  main - prints the lowercase alphabet in reverse.
+*  Return: always 0 (success)
+*
+**/
+int main(void)
+{
+int i;
+for (i = 122; i <= 97; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
