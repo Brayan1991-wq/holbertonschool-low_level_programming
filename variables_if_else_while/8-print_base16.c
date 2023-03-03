37c13c0d30f814af2ba7039af8f2ce57e9c993d5
Brayan1991-wq <brayanmalafaya17@gmail.com>
2023-03-02 16:23:45 -0800

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*  main - Entry point
+*  Return: always o (success)
+*
+**/
+int main(void)
+{
+int n;
+int a;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+for (a = 97; a <= 102; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
