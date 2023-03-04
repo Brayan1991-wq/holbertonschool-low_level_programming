bbabd03c2fb59f226f18bb4277670194a3e427bf
Brayan1991-wq <brayanmalafaya17@gmail.com>
2023-03-03 21:55:43 -0800

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*  main - Entry point
+*  Return: always 0 (success)
+*
+**/
+void print_alphabet(void)
+{
+int a;
+for (a = 97; a <= 122; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
