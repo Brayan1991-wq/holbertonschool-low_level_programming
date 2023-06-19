73fe0e77bec35eb5bd1888ae7ef19dc4eceb70b5
Brayan1991-wq <brayanmalafaya17@gmail.com>
2023-06-19 14:46:32 -0700

    program that prints a line of text

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - second program
+ *
+ * Description: print a line of text
+ *
+ * Return: sucess
+ **/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
